ba8ab2c9696ded6b7552b65f61a63651019ee56b
benny <vvsogi@gmail.com>
2025-04-27 19:07:47 +0900

    Update App component and CSS to initial setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import '@ant-design/v5-patch-for-react-19'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@ant-design/v5-patch-for-react-19'
+import '@/index.css'
+import App from '@/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
