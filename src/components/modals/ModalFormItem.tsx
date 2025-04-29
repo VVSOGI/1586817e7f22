c7fc2425bfe312e5f96892e5bbc0d36c9d4e9ee7
benny <vvsogi@gmail.com>
2025-04-29 16:34:06 +0900

    Refactor ModalFormItem component styling

--- src/components/modals/ModalFormItem.tsx
+++ src/components/modals/ModalFormItem.tsx
@@ -1,5 +1,18 @@
-import { COLORS } from '@/styles'
+import { styled } from 'styled-components'
 import { Typography } from '@/components'
+import { COLORS } from '@/styles'
+
+const FormItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const FormItemTitle = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`
 
 interface Props {
   label: string
@@ -9,12 +22,12 @@ interface Props {
 
 export function ModalFormItem({ label, required, children }: Props) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div style={{ height: '40px', display: 'flex', alignItems: 'center', gap: '4px' }}>
+    <FormItemContainer>
+      <FormItemTitle style={{ height: '40px', display: 'flex', alignItems: 'center', gap: '4px' }}>
         <Typography.h1 style={{ color: COLORS.GRAY_600 }}>{label}</Typography.h1>
         {required && <Typography.h1 style={{ color: COLORS.ERROR, fontWeight: 400 }}>*</Typography.h1>}
-      </div>
+      </FormItemTitle>
       {children}
-    </div>
+    </FormItemContainer>
   )
 }
