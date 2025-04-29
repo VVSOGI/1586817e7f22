fd7fef3d3adfb25311a3a0866c16dafa55c15733
benny <vvsogi@gmail.com>
2025-04-29 11:39:51 +0900

    Rename user management hooks to use storage management

--- src/hooks/useStorageManagement.ts
+++ src/hooks/useStorageManagement.ts
@@ -1,18 +1,24 @@
 import { v4 } from 'uuid'
 import { useRef, useState } from 'react'
-import { createLocalStorage, getStorageType } from '@/utils'
-import { StorageInterface, User } from '@/types'
+import { createLocalStorage, formatDate, getStorageType } from '@/utils'
+import { PreprocessedUser, StorageInterface, User } from '@/types'
 import { mockUsers } from '@/configs'
 
-export function useUserManagement() {
+/**
+ * getStorageType 함수 리턴값에 따른 local-storage 이용 여부 결정
+ * in-memory 이용시 편의성 mockUsers 반환.
+ * Storage에 CRUD 기능이 있기에 좀 더 명확하게 Management라는 네이밍 추가
+ */
+export function useStorageManagement() {
   const storageRef = useRef<StorageInterface | null>(getStorageType() === 'local-storage' ? createLocalStorage() : null)
   const [users, setUsers] = useState<User[]>(() => {
     return storageRef.current ? storageRef.current.getAll() : [...mockUsers]
   })
 
-  const addUser = (userData: Omit<User, 'id'>) => {
+  const handleAdd = (userData: PreprocessedUser) => {
     const newUser: User = {
       id: v4(),
+      updatedAt: formatDate(new Date()),
       ...userData
     }
 
@@ -25,7 +31,7 @@ export function useUserManagement() {
     return newUser
   }
 
-  const updateUser = (updated: User) => {
+  const handleUpdate = (updated: User) => {
     setUsers((prev) => {
       return prev.map((user) => {
         if (user.id === updated.id) {
@@ -43,7 +49,7 @@ export function useUserManagement() {
     })
   }
 
-  const deleteUser = (id: string) => {
+  const handleDelete = (id: string) => {
     setUsers((prev) => prev.filter((user) => user.id !== id))
 
     if (storageRef.current) {
@@ -53,8 +59,8 @@ export function useUserManagement() {
 
   return {
     users,
-    addUser,
-    updateUser,
-    deleteUser
+    handleAdd,
+    handleUpdate,
+    handleDelete
   }
 }
